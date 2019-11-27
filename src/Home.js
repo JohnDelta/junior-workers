d05578c045ca5798c91c068c2e374e6c43867948
John Delta <john.deligiannis1@gmail.com>
2019-11-28 01:02:52 +0200

    add to home page

--- src/Home.js
+++ src/Home.js
@@ -20,13 +20,37 @@ class Home extends React.Component{
             <span>Your professional</span>
             <span>career starts</span>
             <span>here</span>
+            <span>
+              <a href="#infos"><i className="fa fa-arrow-down"></i></a>
+            </span>
           </h1>
-          <img className="image" src="" />
+          <img className="image" src={require(`./images/background_image.png`)} />
           <button className="join-button">Join our community</button>
           <div className="main-circle" />
         </div>
 
-        <div className="info"></div>
+        <div className="infos" id="infos">
+          <h2 className="infos-title">Use Junior Workers and find what you're looking for</h2>
+          <div className="info">
+            <div className="info-title">Recruiters<br/>can</div>
+            <div className="info-uses">
+              <div className="info-use"><i className="fa fa-circle" />Search workers</div>
+              <div className="info-use"><i className="fa fa-circle" />Create a job post</div>
+            </div>
+          </div>
+          
+          <div className="info">
+            <div className="info-title" style={{"backgroundColor": "#428438"}}>Workers<br/>can</div>
+            <div className="info-uses">
+              <div className="info-use"><i className="fa fa-circle" />Search workers</div>
+              <div className="info-use"><i className="fa fa-circle" />Create a job post</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer">
+          asdf
+        </div>
       </div>
     );
   }
