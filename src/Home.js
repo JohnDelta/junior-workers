f237e9014f0e0c658c4c82bff66496d6a4853190
John Delta <john.deligiannis1@gmail.com>
2019-11-30 01:38:36 +0200

    add adapting behavior in home page

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,19 @@ class Home extends React.Component{
     return(
       <div className="Home">
         
-        <div className="header">
-          <div className="title">
-            <span>Junior</span>
-            <span style={{"marginLeft":"15px"}}>Workers</span>
-          </div>
+        <div className="logo">
+          <div className="title1">Junior</div>
+          <div className="title2">Workers</div>
+          <div className="logo-circle" />
+        </div>
+
+        <div className="join">
+          <button className="join-button">Join our community</button>
+          <div className="join-circle" />
+        </div>
+
+        <div className="login">
           <button className="login-button">Login</button>
-          <div className="header-circle" />
         </div>
         
         <div className="main">
@@ -20,36 +26,38 @@ class Home extends React.Component{
             <span>Your professional</span>
             <span>career starts</span>
             <span>here</span>
-            <span>
-              <a href="#infos"><i className="fa fa-arrow-down"></i></a>
-            </span>
+            <input className="search-bar" type="text" placeholder="type something..." />
           </h1>
           <img className="image" src={require(`./images/background_image.png`)} />
-          <button className="join-button">Join our community</button>
-          <div className="main-circle" />
         </div>
 
         <div className="infos" id="infos">
-          <h2 className="infos-title">Use Junior Workers and find what you're looking for</h2>
+          <h2 className="infos-title">Use Junior Workers and find your place in job industry</h2>
           <div className="info">
             <div className="info-title">Recruiters<br/>can</div>
             <div className="info-uses">
-              <div className="info-use"><i className="fa fa-circle" />Search workers</div>
-              <div className="info-use"><i className="fa fa-circle" />Create a job post</div>
+              <div className="info-use">Search workers</div>
+              <div className="info-use">Create a job post</div>
             </div>
           </div>
-          
           <div className="info">
-            <div className="info-title" style={{"backgroundColor": "#428438"}}>Workers<br/>can</div>
+            <div className="info-title">Workers<br/>can</div>
             <div className="info-uses">
-              <div className="info-use"><i className="fa fa-circle" />Search workers</div>
-              <div className="info-use"><i className="fa fa-circle" />Create a job post</div>
+              <div className="info-use">Search job posts</div>
+              <div className="info-use">Create a professional profil</div>
             </div>
           </div>
         </div>
 
         <div className="footer">
-          asdf
+          Designed and Coded by
+          <a 
+						href="https://github.com/JohnDelta"
+						target="_blank" 
+						title="Visit my github profile!"
+						rel="noopener noreferrer" >
+              John Deligiannis 
+          </a>
         </div>
       </div>
     );
