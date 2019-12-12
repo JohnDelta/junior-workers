c96edb4a37cfe3566378ae00f1a214538c28363d
John Delta <john.deligiannis1@gmail.com>
2019-12-13 01:58:14 +0200

    add navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,25 +3,41 @@ import {
   BrowserRouter as Router,
   Link
 } from "react-router-dom";
-import './App.css';
+import './Navbar.css';
 
 class Navbar extends React.Component{
+  
   render() {
+    var linkActive = {color: "#fff1c1"};
+
     return(
       <div className="Navbar">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+        <ul className="nav-links">
+          <li className="nav-link">
+            <i className="fa fa-home link-active" />
+            <Link className="text link-active" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-link">
+            <i className="fa fa-user" />
+            <Link className="text" to="/" >
+              Profil
+            </Link>
+          </li>
+          <li className="nav-link">
+            <i className="fa fa-comments" />
+            <Link className="text" to="/" >
+              Messages
+            </Link>
+          </li>
+          <li className="nav-link nav-down">
+            <i className="fa fa-sign-out" />
+            <Link className="text" to="/" >
+              Exit
+            </Link>
+          </li>
+        </ul>
       </div>
     );
   }
--- src/Profil.js
+++ src/Profil.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Profil.css';
+import Navbar from './Navbar';
 
 class Profil extends React.Component {
     constructor(props) {
@@ -9,7 +10,16 @@ class Profil extends React.Component {
     render() {
         return(
             <div className="Profil">
-                profil
+                <img id="bg" className="background" src={require('./images/backgroundBig.jpg')} />
+
+                <div className="logo">
+                    <div className="title1">Junior</div>
+                    <div className="title2">Workers</div>
+                    <div className="logo-circle" />
+                </div>
+
+                <Navbar />
+
             </div>
         );
     }
