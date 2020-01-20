2b41036ed37882dc77e51a2e0c1736260c9263c9
John Delta <john.deligiannis1@gmail.com>
2020-01-20 21:05:09 +0200

    Updates

---
+++ precache-manifest.888ddbdee108470534b6c42b372edf3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd61be646f69d65f61f210d09e5142f3",
+    "url": "/junior-workers/index.html"
+  },
+  {
+    "revision": "eabc8301b4bc4135b987",
+    "url": "/junior-workers/static/css/main.e3d9fd7b.chunk.css"
+  },
+  {
+    "revision": "a5bb716d49da76a620a4",
+    "url": "/junior-workers/static/js/2.6392d5cc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/junior-workers/static/js/2.6392d5cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eabc8301b4bc4135b987",
+    "url": "/junior-workers/static/js/main.14efcafb.chunk.js"
+  },
+  {
+    "revision": "53540a6df2895ac6ba43",
+    "url": "/junior-workers/static/js/runtime-main.313a8c36.js"
+  },
+  {
+    "revision": "f2b86eb104afc67f4d07eaf9b24abe46",
+    "url": "/junior-workers/static/media/backgroundBig.f2b86eb1.jpg"
+  },
+  {
+    "revision": "afdbf1d8e21a8e6715aab5aab21592eb",
+    "url": "/junior-workers/static/media/main_image.afdbf1d8.png"
+  }
+]);
